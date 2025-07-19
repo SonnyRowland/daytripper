e0f199c26aa9e0014fa75bc63c26ce84fbaed81d
Sonny Rowland <sonnyrowland@gmail.com>
2025-07-19 21:23:24 +0100

    resolve merge conflict in db url

--- app/config.py
+++ app/config.py
@@ -8,10 +8,7 @@ load_dotenv()
 class Settings:
     def __init__(self):
         self.database_url = os.getenv(
-            "database_url", "postgresql://localhost:5432/tripper_db"
-        )
-        self.postcode_api_url = os.getenv(
-            "POSTCODE_API_URL", "https://api.postcodes.io/postcodes/"
+            "database_url", "postgresql://localhost:5432/daytripper_db"
         )
         self.postcode_api_url = os.getenv(
             "POSTCODE_API_URL", "https://api.postcodes.io/postcodes/"
