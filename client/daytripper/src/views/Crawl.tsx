a537d6f0b98d4684c4eda7ea4bf490628585c0ef
Sonny Rowland <sonnyrowland@gmail.com>
2025-09-17 11:34:47 +0100

    add mock images and opening times for demo purposes

--- client/daytripper/src/views/Crawl.tsx
+++ client/daytripper/src/views/Crawl.tsx
@@ -4,9 +4,10 @@ import axios from "axios";
 import { Icon } from "@iconify/react";
 
 import type { PubType } from "@/types";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Header } from "@/components/Header";
 import { Spinner } from "@/components/ui/shadcn-io/spinner";
+import ImageCard from "@/components/ui/image-card";
 import { ErrorFetchingData } from "./ErrorFetchingData";
 
 export const Crawl = () => {
@@ -48,17 +49,20 @@ export const Crawl = () => {
   return (
     <Header>
       <div className="flex flex-col h-full items-center gap-[16px] p-[16px]">
-        {data.map((pub) => {
+        {data.map((pub, index) => {
           return (
-            <Card className="w-[80%]">
-              <CardHeader>
-                <CardTitle>{pub.name}</CardTitle>
-              </CardHeader>
+            <Card className="w-[80%]" key={index}>
               <CardContent>
-                <div className="flex flex-col gap-[16px]">
-                  {pub.address}
-
-                  <div className="flex w-full justify-end">
+                <div className="flex flex-col gap-[16px] w-full">
+                  <ImageCard
+                    className="w-[200px] self-center"
+                    caption={pub.name}
+                    imageUrl={`/assets/images/${index + 1}.jpg`}
+                  />
+                  <div className="flex w-full justify-between">
+                    <div className="flex flex-col justify-center">
+                      Open until {`2${Math.floor(Math.random() * 3) + 1}:00`}
+                    </div>
                     <a
                       href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
                         `${pub.name} ${pub.postcode}`
